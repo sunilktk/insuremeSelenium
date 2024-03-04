7bdd510e6ea3abcfa85db4ece829ca9c3f77ba13
Sunil <sunil@test.com>
2024-03-04 14:45:56 -0500

    updated Test server IP

--- src/main/java/com/seleniumInsurance/seleniumInsurance/App.java
+++ src/main/java/com/seleniumInsurance/seleniumInsurance/App.java
@@ -29,7 +29,7 @@ public class App
 	  //1. Open the browser and get to the url
 	  //driver.get("http://localhost:8081/contact.html");	
 	  //Configured test server url
-	  driver.get("http://3.14.255.82:8083//contact.html");	
+	  driver.get("http://3.138.126.239:8083//contact.html");	
 	  //enable wait of 3 seconds 
 	  driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);	  
 	  driver.findElement(By.id("inputName")).sendKeys("SeleniumTestServer");
